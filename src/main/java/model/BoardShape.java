0684845b9c1e3c3a4f7eb02573bcbbd8367441dc
lemonpesto <potatobanana050129@gmail.com>
2025-05-09 01:41:01 +0900

    파일 생성 및 정의

---
+++ src/main/java/model/BoardShape.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * 윷놀이판의 형태를 나타내는 enum
+ */
+public enum BoardShape {
+    /** 정사각형 판 */
+    SQUARE,
+    /** 오각형 판 (추후 구현) */
+    PENTAGON,
+    /** 육각형 판 (추후 구현) */
+    HEXAGON
+}
