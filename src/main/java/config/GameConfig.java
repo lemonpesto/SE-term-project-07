1ae567474b2abc2d294cad59501657f31472c40b
narin1010 <rlaskfls1@cau.ac.kr>
2025-05-09 00:29:00 +0900

    5/8 작성중

--- src/main/java/config/GameConfig.java
+++ src/main/java/config/GameConfig.java
@@ -1,4 +1,11 @@
 package config;
 
-public class GameConfig {
+public class GameConfig{
+    int numPlayers;
+    int piecesPerPlayer; //플레이어의 말 개수
+    BoardShape[] boardShape = new BoardShape[]{SQUARE, PENTAGON, HEXAGON};
+
+    Boolean validate(){
+        //설정값 검증
+    }
 }
